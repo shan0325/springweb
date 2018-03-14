be38fdac7cc1bfda17c4523b0606881e09a17a55
shan0325 <shan0325@naver.com>
2018-03-14 18:24:01 +0900

    c

--- src/main/java/com/shan/app/domain/Authority.java
+++ src/main/java/com/shan/app/domain/Authority.java
@@ -1,9 +1,16 @@
 package com.shan.app.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
@@ -31,5 +38,12 @@ public class Authority {
 	
 //	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "authorities")
 //	private Set<Admin> admins = new HashSet<>();
+	
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(
+		name = "tb_authority_menu",
+		joinColumns = @JoinColumn(name = "authority_id"),
+		inverseJoinColumns = @JoinColumn(name = "menu_id"))
+	private List<Menu> menus = new ArrayList();
 
 }
---
+++ src/main/java/com/shan/app/domain/Menu.java
@@ -0,0 +1,62 @@
+package com.shan.app.domain;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_menu")
+public class Menu {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(name = "parent_id", nullable = false)
+	private Long parentId;
+	
+	@Column(name = "top_id", nullable = false)
+	private Long topId;
+	
+	@Column(name = "group_id", nullable = false)
+	private Long groupId;
+	
+	@Column(length = 100, nullable = false)
+	private String name;
+	
+	private String description;
+	
+	@Column(name = "menu_image_path")
+	private String menuImagePath;
+	
+	@Column(name = "use_yn", length = 1, nullable = false)
+	private String useYn;
+	
+	@Column(name = "menu_type", nullable = false)
+	private String menuType;
+	
+	@Column(name = "url_type", nullable = false)
+	private String urlType;	//local(/menu/..), external(http://..)
+	
+	@Column(nullable = false)
+	private String url;
+	
+	@Column(name = "url_target", nullable = false)
+	private String urlTarget;
+	
+	@Column(nullable = false)
+	private Integer ord;
+	
+	@Column(name = "reg_date")
+	private Date regDate;
+	
+	@Column(name = "update_date")
+	private Date updateDate;
+}
