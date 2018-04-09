23417a82276f6372a046364636509a9e6ada6fee
shan0325 <shan0325@naver.com>
2018-04-09 18:00:35 +0900

    c

--- src/main/java/com/shan/app/repository/cms/impl/MenuRepositoryImpl.java
+++ src/main/java/com/shan/app/repository/cms/impl/MenuRepositoryImpl.java
@@ -1,7 +1,9 @@
 package com.shan.app.repository.cms.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.criterion.Order;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.jpa.repository.support.QueryDslRepositorySupport;
@@ -23,10 +25,75 @@ public class MenuRepositoryImpl extends QueryDslRepositorySupport implements Men
 	
 	@Override
 	public List<Menu> findAllByQueryDsl() {
-		QMenu menu = QMenu.menu;
+		QMenu qMenu = QMenu.menu;
 		
-		JPQLQuery<Menu> query = from(menu);
-		return query.fetch();
+		JPQLQuery<Menu> query = from(qMenu);
+		List<Menu> menus = query.fetch();
+		
+		JPQLQuery<Menu> query2 = from(qMenu);
+		List<Menu> parMenus = query2.where(qMenu.depth.eq(0)).orderBy(qMenu.ord.asc(), qMenu.id.asc()).fetch();
+		
+		List<Menu> hirMenus = new ArrayList<Menu>();
+		Menu execMenu = new Menu();
+		boolean ing = true;
+		for(Menu parMenu : parMenus) {
+			hirMenus.add(parMenu);
+			
+			execMenu = parMenu;
+			int curOrd = 0;
+			int tempOrd = 0;
+			ing = true;
+			while(ing) {
+				Menu child = null;
+				int index = 0;
+				for(Menu menu : menus) {
+					if(execMenu.getId() == menu.getParentId() &&  menu.getOrd() > curOrd) {
+						if (index == 0) {
+							tempOrd = menu.getOrd();
+							child = menu;
+						}
+						
+						if(menu.getOrd() < tempOrd) {
+							child = menu;
+						}
+						index++;
+					}
+				}
+				logger.debug("child = " + child);
+				curOrd = 0;
+				tempOrd = 0;
+				
+				if(child != null) {
+					hirMenus.add(child);
+					execMenu = child;
+					continue;
+				} else {
+					for(Menu menu : menus) {
+						if(execMenu.getParentId() == menu.getId()) {
+							curOrd = execMenu.getOrd();
+							execMenu = menu;
+						}
+					}
+				}
+				
+				int breakCnt = 0;
+				if(execMenu.getParentId() == 0) {
+					for(Menu menu : menus) {
+						if(execMenu.getId() == menu.getParentId()) {
+							if(curOrd < menu.getOrd()) {
+								breakCnt++;
+							}
+						}
+					}
+					
+					if(breakCnt == 0) ing = false;
+				}
+			}
+			
+		}
+		
+		logger.debug("hirMenus = " + hirMenus);
+		return hirMenus;
 	}
 
 }
