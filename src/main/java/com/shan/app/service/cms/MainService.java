d32a8308f7fcd87e1a9ebee352e1883dc2942b3e
ykc\shan <shan0325@naver.com>
2018-05-09 23:55:30 +0900

    h

--- src/main/java/com/shan/app/service/cms/MainService.java
+++ src/main/java/com/shan/app/service/cms/MainService.java
@@ -6,13 +6,16 @@ import java.util.List;
 import javax.annotation.Resource;
 import javax.transaction.Transactional;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
+import com.shan.app.domain.Authority;
 import com.shan.app.domain.Menu;
+import com.shan.app.repository.cms.AuthorityRepository;
 import com.shan.app.repository.cms.MenuRepository;
 import com.shan.app.service.util.SecurityUtil;
 
@@ -25,20 +28,30 @@ public class MainService {
 	@Resource(name="cmsMenuRepository")
 	private MenuRepository menuRepository;
 	
+	@Resource(name="cmsAuthorityRepository")
+	private AuthorityRepository authorityRepository;
+	
 
 	public String getSystemRedirectUrl(Long systemMenuId) {
-		
-		String authority = "";
 		List<Menu> menus = menuRepository.findQueryDslHierarchicalMenuById(systemMenuId);
 		
+		String authorityName = "";
 		User user = SecurityUtil.getUser();
-		Iterator iterator = user.getAuthorities().iterator();
+		Iterator<GrantedAuthority> iterator = user.getAuthorities().iterator();
 		while(iterator.hasNext()) {
-			GrantedAuthority grantedAuthority = (GrantedAuthority) iterator.next();
-			authority = grantedAuthority.getAuthority();
+			GrantedAuthority grantedAuthority = iterator.next();
+			authorityName = grantedAuthority.getAuthority();
 		}
 		
-		
+		if(StringUtils.isEmpty(authorityName)) {
+			return "";
+		} else {
+			Authority authority = authorityRepository.findOneByAuthority(authorityName);
+			
+			for(Menu menu : menus) {
+				
+			}
+		}
 		
 		return null;
 	}
